ff4d4ebc360e625edd2feae4f15877388c47a2ea
zhouqiong <zhouqiong@beescm.cn>
2018-09-14 18:12:25 +0800

    优化我的界面和分类界面，修改bug，完善业务逻辑

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,61 +1,189 @@
 // pages/category/category.js
+var app = getApp();
+var tabs = [{
+    name: "all"
+  },
+  {
+    name: "福利"
+  },
+  {
+    name: "前端"
+  },
+  {
+    name: "Android"
+  },
+  {
+    name: "iOS"
+  }, {
+    name: "瞎推荐"
+  },
+  {
+    name: "拓展资源"
+  },
+  {
+    name: "休息视频"
+  },
+  {
+    name: "App"
+  }
+];
 Page({
 
   /**
    * 页面的初始数据
    */
+
   data: {
-    menus: [{
-        "name": "all",
-        "url": '../../images/menu/all.png'
-      },
-      {
-        "name": "iOS",
-        "url": '../../images/menu/ios.png'
-      },
-      {
-        "name": "Android",
-        "url": '../../images/menu/android.png'
-      },
-      {
-        "name": "前端",
-        "url": '../../images/menu/frontend.png'
-      },
-      {
-        "name": "瞎推荐",
-        "url": '../../images/menu/recomm.png'
-      },
-      {
-        "name": "拓展资源",
-        "url": '../../images/menu/resource.png'
-      },
-      {
-        "name": "App",
-        "url": '../../images/menu/app.png'
-      },
-      {
-        "name": "休息视频",
-        "url": '../../images/menu/video.png'
-      },
-      {
-        "name": "福利",
-        "url": '../../images/menu/meizi.png'
-      }
-    ]
+    winHeight: "", //窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    isLoadingMore: false,
+    currentPage: 1, // 当前页数  默认是1
+    titleInfo: 'all',
+    contentlist: [], // 列表显示的数据源
+    tabs: tabs, //展示的数据
   },
 
-  //item的点击事件
-  handleTapMiddle: function(event) {
-    var $title = event.currentTarget.dataset.title;
-    console.log($title);
-    if ($title == '福利') {
-      wx.navigateTo({
-        url: '../wetify/wetify?title=' + $title,
+  // 滚动切换标签样式
+  switchTab: function(e) {
+    var cur = e.detail.current;
+    this.setData({
+      currentTab: cur,
+      titleInfo: tabs[cur].name
+    });
+    this.checkCor();
+    var that = this;
+    that.loadingData();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function(e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur,
+        titleInfo: tabs[cur].name
+      })
+    }
+    var that = this;
+    that.loadingData();
+  },
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function() {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
       })
     } else {
-      wx.navigateTo({
-        url: '../detail/detail?title=' + $title,
+      this.setData({
+        scrollLeft: 0
       })
     }
   },
+
+  onLoad: function() {
+    var that = this;
+    that.loadingData();
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function(res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR - 180;
+        console.log(calc)
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
+  },
+
+  /**
+   * 刷新
+   */
+  onPullRefresh: function() {
+    // Do something when pull down.
+    console.log('--------下拉刷新-------');
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    var that = this;
+    that.setData({
+      currentPage: 1,
+      isLoadingMore: false
+    })
+    that.loadingData();
+  },
+
+  /**
+   * 加载更多
+   */
+  loadMore: function() {
+    // Do something when page reach bottom.
+    console.log('circle 下一页');
+    var self = this;
+
+    var tempCurrentPage = self.data.currentPage;
+    tempCurrentPage = tempCurrentPage + 1;
+    self.setData({
+      currentPage: tempCurrentPage,
+      isLoadingMore: false
+    })
+    self.loadingData();
+  },
+  /**
+   * 获取数据  pageIndex：页码参数
+   */
+  loadingData: function() {
+    wx.showLoading({
+      title: '',
+    })
+    var self = this;
+    var pageIndex = self.data.currentPage;
+    var url = 'https://gank.io/api/data/' + self.data.titleInfo + '/10/' + pageIndex;
+    console.log(url)
+    wx.request({
+      url: url,
+      success: function(res) {
+        console.log(res.data)
+        wx.hideLoading()
+        wx.stopPullDownRefresh()
+        wx.hideNavigationBarLoading()
+        if (pageIndex == 1) { //下拉刷新
+          self.setData({
+            contentlist: res.data.results,
+          })
+        } else { //加载更多
+          var tempArray = self.data.contentlist;
+          tempArray = tempArray.concat(res.data.results);
+          self.setData({
+            contentlist: tempArray,
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 点击事件
+   */
+  itemClick: function(event) {
+    var $newsUrl = event.currentTarget.dataset.url;
+    console.log($newsUrl);
+    wx.navigateTo({
+      //跳转到详情
+      url: '../webdetail/webdetail?newsUrl=' + $newsUrl,
+    })
+  },
+    /**
+   * 点击事件
+   */
+  onClick: function (event) {
+    var $imgPath = event.currentTarget.dataset.url;
+    var arrayObj = new Array();
+    arrayObj.push($imgPath);
+    wx.previewImage({
+      current: $imgPath, // 当前显示图片的http链接
+      urls: arrayObj // 需要预览的图片http链接列表
+    })
+  }
 })
--- pages/me/me.js
+++ pages/me/me.js
@@ -5,47 +5,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-    ListInfo: [{
-      icon: '../../dist/images/iconfont-dingdan.png',
-      text: '干货推荐',
-      url: '',
-      isunread: false,
-      unreadNum: 2
-    }, {
-      icon: '../../dist/images/iconfont-card.png',
-      text: '感谢编辑们',
-      url: 'http://gank.io/backbone',
-      isunread: false,
-      unreadNum: 2
-    }, {
-      icon: '../../dist/images/iconfont-icontuan.png',
-      text: '关于',
-      url: '',
-      isunread: false,
-      unreadNum: 1
-    }, {
-      icon: '../../dist/images/iconfont-shouhuodizhi.png',
-      text: '版本更新',
-      url: ''
-    }, {
-      icon: '../../dist/images/iconfont-kefu.png',
-      text: 'Gank 的IOS版',
-      url: 'https://github.com/ZQ330093887/GankIOSProgect'
-    }, {
-      icon: '../../dist/images/iconfont-help.png',
-      text: 'Gank 的Android版',
-      url: 'https://github.com/ZQ330093887/ConurbationsAndroid'
-    }],
+    menus: [{
+        "name": "all",
+        "url": '../../images/menu/all.png'
+      },
+      {
+        "name": "iOS",
+        "url": '../../images/menu/ios.png'
+      },
+      {
+        "name": "App",
+        "url": '../../images/menu/app.png'
+      },
+      {
+        "name": "Android",
+        "url": '../../images/menu/android.png'
+      },
+      {
+        "name": "前端",
+        "url": '../../images/menu/frontend.png'
+      },
+      {
+        "name": "瞎推荐",
+        "url": '../../images/menu/recomm.png'
+      },
+      {
+        "name": "拓展资源",
+        "url": '../../images/menu/resource.png'
+      },
+      {
+        "name": "休息视频",
+        "url": '../../images/menu/video.png'
+      },
+      {
+        "name": "福利",
+        "url": '../../images/menu/meizi.png'
+      }
+    ],
+
+    menus01: [{
+        "name": "干货推荐",
+        "url": '../../images/menu/tuijian.png'
+      },
+      {
+        "name": "感谢编辑",
+        "url": '../../images/menu/bianji.png'
+      },
+      {
+        "name": "版本更新",
+        "url": '../../images/menu/genxin.png'
+      },
+      {
+        "name": "关于作者",
+        "url": '../../images/menu/zuozhe.png'
+      }
+    ],
     winHeight: 0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let that = this;
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         let winHeight = res.windowHeight;
         that.setData({
           winHeight: winHeight
@@ -53,62 +78,73 @@ Page({
       },
     })
   },
-
-  onClick: function (event) {
-    var $title = event.currentTarget.dataset.title;
-    var $newsUrl = event.currentTarget.dataset.url;
-    console.log($newsUrl);
-    if ($title == "干货推荐") {
-      wx.navigateTo({
-        url: '../push/push?newsUrl=' + $title,
-      })
-    } else if ($title == "感谢编辑们") {
-      wx.navigateTo({
-        url: '../webdetail/webdetail?newsUrl=' + $newsUrl,
-      })
-    } else if ($title == "Gank 的IOS版") {
-      wx.navigateTo({
-        url: '../webdetail/webdetail?newsUrl=' + $newsUrl,
-      })
-    } else if ($title == "Gank 的Android版") {
-      wx.navigateTo({
-        url: '../webdetail/webdetail?newsUrl=' + $newsUrl,
-      })
-    } else if ($title == "关于") {
-      wx.navigateTo({
-        url: '../about/about?newsUrl=' + $title,
-      })
-    } else if ($title == "版本更新") {
-      wx.navigateTo({
-        url: '../update/update?newsUrl=' + $title,
-      })
-    }
-  },
-  login: function (event){
+  
+  login: function(event) {
     wx.navigateTo({
       url: '../login/login?newsUrl',
     })
 
   },
-  // /**
-  //  * 用户点击右上角分享
-  //  */
-  // onShareAppMessage: function (ops) {
-  //   if (ops.id === 'shareBtn') {
-  //     // 来自页面内转发按钮
-  //     console.log(ops.target)
-  //   }
-  //   return {
-  //     title: 'xx小程序',
-  //     path: 'pages/index/index',
-  //     success: function (res) {
-  //       // 转发成功
-  //       console.log("转发成功:" + JSON.stringify(res));
-  //     },
-  //     fail: function (res) {
-  //       // 转发失败
-  //       console.log("转发失败:" + JSON.stringify(res));
-  //     }
-  //   }
-  // }
+  wxClick: function(event) {
+    console.log(event.currentTarget.dataset.name)
+
+    var nUrl = "https://github.com/ZQ330093887/GankWX";
+    wx.navigateTo({
+      url: '../webdetail/webdetail?newsUrl=' + nUrl,
+    })
+  },
+  wxflutter: function(event) {
+    console.log(event.currentTarget.dataset.name)
+
+    var nUrl = "https://github.com/ZQ330093887/GankFlutter";
+    wx.navigateTo({
+      url: '../webdetail/webdetail?newsUrl=' + nUrl,
+    })
+  },
+  wxAndroid: function(event) {
+    console.log(event.currentTarget.dataset.name)
+
+    var nUrl = "https://github.com/ZQ330093887/ConurbationsAndroid";
+    wx.navigateTo({
+      url: '../webdetail/webdetail?newsUrl=' + nUrl,
+    })
+  },
+  wxIOS: function(event) {
+    console.log(event.currentTarget.dataset.name)
+
+    var nUrl = "https://github.com/ZQ330093887/GankIOSProgect";
+    wx.navigateTo({
+      url: '../webdetail/webdetail?newsUrl=' + nUrl,
+    })
+  },
+  //item的点击事件
+  handleTapMiddle: function(event) {
+    var $title = event.currentTarget.dataset.title;
+    console.log($title);
+    if ($title == '福利') {
+      wx.navigateTo({
+        url: '../wetify/wetify?title=' + $title,
+      })
+    } else if ($title == '干货推荐') {
+      wx.navigateTo({
+        url: '../push/push?newsUrl=' + $title,
+      })
+    } else if ($title == '感谢编辑') {
+      wx.navigateTo({
+        url: '../webdetail/webdetail?newsUrl=http://gank.io/backbone',
+      })
+    } else if ($title == '版本更新') {
+      wx.navigateTo({
+        url: '../update/update?newsUrl=' + $title,
+      })
+    } else if ($title == '关于作者') {
+      wx.navigateTo({
+        url: '../about/about?newsUrl=' + $title,
+      })
+    }else {
+      wx.navigateTo({
+        url: '../detail/detail?title=' + $title,
+      })
+    }
+  },
 })
